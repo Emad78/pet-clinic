8517e441388a1f066378fea3e294376c37ce393d
mehdi mohajeri <mehdimohajeri78@gmail.com>
2021-10-15 03:04:09 +0330

    add findPet test

--- src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
@@ -1,11 +1,38 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.hibernate.annotations.Parameter;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.samples.petclinic.utility.SimpleDI;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
+@RunWith(Parameterized.class)
+public class PetServiceTest {
 
-class PetServiceTest {
+	public Pet expected;
+	public PetService petService;
 
+	public PetServiceTest( int petId){
+		String petName = "hi";
+		Pet pet = new Pet();
+		pet.setId(petId);
+		pet.setName(petName);
+		Owner owner = new Owner();
+		petService.savePet(pet, owner);
+		expected = pet;
+	}
+	@Parameterized.Parameters
+	public static Collection<Object[]> parameters(){return Arrays.asList(new Object[][]{{1}, {2}});
+	}
+	@Test public void findPetTest(){
+		assertEquals(expected, petService.findPet(expected.getId()));
+	}
 
 }
