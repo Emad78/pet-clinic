bd631f486644dc7a35f46fe7c422464c3023f765
Sina Pakseresht <sina.pakseresht@divar.ir>
2021-11-11 09:13:47 +0330

    Refactor a little

--- src/test/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculatorTests.java
@@ -1,36 +1,36 @@
-package org.springframework.samples.petclinic.model;
+package org.springframework.samples.petclinic.model.priceCalculators;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.samples.petclinic.model.priceCalculators.SimplePriceCalculator;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.UserType;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-class calcPriceEdgeCoverageTest {
+class SimplePriceCalculatorTests {
 
-	private Pet pet1;
-	private Pet pet2;
 	private List<Pet> pets;
 	private double baseCharge;
 	private double basePricePerPet;
 
 	@BeforeEach
-	public void setup(){
-		this.pets = new ArrayList<Pet>();
+	public void setup() {
+		this.pets = new ArrayList<>();
 		this.baseCharge = 200;
 		this.basePricePerPet = 1000;
 	}
 
 	@Test
-	public void calcPriceEdgeCover_path1(){
+	public void calcPriceEdgeCover_path1() {
 		double totalPrice;
 		SimplePriceCalculator priceCalculator = new SimplePriceCalculator();
 		totalPrice = priceCalculator.calcPrice(this.pets, this.baseCharge, this.basePricePerPet, UserType.GOLD);
@@ -38,9 +38,9 @@ class calcPriceEdgeCoverageTest {
 	}
 
 	@Test
-	public void calcPriceEdgeCover_path2(){
-		pet1 = mock(Pet.class);
-		pet2 = mock(Pet.class);
+	public void calcPriceEdgeCover_path2() {
+		Pet pet1 = mock(Pet.class);
+		Pet pet2 = mock(Pet.class);
 		PetType petType1 = mock(PetType.class);
 		PetType petType2 = mock(PetType.class);
 		when(pet1.getType()).thenReturn(petType1);
@@ -50,9 +50,15 @@ class calcPriceEdgeCoverageTest {
 		pets.add(pet1);
 		pets.add(pet2);
 		double totalPrice, expected;
-		expected = (this.baseCharge + this.basePricePerPet*1.2 + this.basePricePerPet) * UserType.NEW.discountRate;
+		expected = (this.baseCharge + this.basePricePerPet * 1.2 + this.basePricePerPet) * UserType.NEW.discountRate;
 		SimplePriceCalculator priceCalculator = new SimplePriceCalculator();
 		totalPrice = priceCalculator.calcPrice(this.pets, this.baseCharge, this.basePricePerPet, UserType.NEW);
 		assertEquals(expected, totalPrice);
 	}
+
+	@Test
+	public void calcPriceFullCoverage() {
+
+	}
+
 }
